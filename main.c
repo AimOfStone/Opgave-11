c97d08b197ca67fff1506e2cde31c90dc6c90d02
AimOfStone <frank.rasmusen@gmail.com>
2025-11-24 16:27:53 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,130 @@
+/*=============================================================================
+*                                   Playlist
+*              https://c-programming.aydos.de/week11/playlist.html
+*  gcc .\opgave-11\main.c .\opgave-11\single_linked_list.c -o
+* .\opgave-11\balls.exe
+*==============================================================================*/
+
+
+#include "singly_linked_list.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TRACK_TITLE_SIZE 60
+#define PLAYLIST_IN_PATH "playlist.txt"
+#define PLAYLIST_OUT_PATH "playlist-out.txt"
+
+// To avoid unnecessary complexity, we fix the filenames instead of getting them
+// through runtime parameters.
+
+typedef char Data[TRACK_TITLE_SIZE];
+Node *playlist;
+
+/// Removes trailing newline from the line, if it exists.
+/// Note: Some lines may not have a newline, e.g., last line in a file,
+/// therefore we have to check for presence.
+/*=============================================================================
+*                              Helper functions
+*=============================================================================*/
+char *remove_newline_if_exists(char *line) {
+	size_t len = strlen(line);
+	if (len > 0 && line[len -1] == '\n')
+	{
+		line[len-1] = "\0";
+	}
+	if ((len > 0 && line[len -2] == '\r'))
+	{
+		line[len-2] = "\0";
+	}
+	return line;
+	}
+
+	/// Reads lines from at `filename`, creates a node for each line and inserts
+	/// nodes to `list`.
+
+Node **load_file(const char *filename, Node **list) {
+	FILE *f = fopen(filename, "r");
+	if (!f) {
+		perror(PLAYLIST_IN_PATH);
+		exit(EXIT_FAILURE);
+	}
+	char line[TRACK_TITLE_SIZE];
+
+	while (fgets(line, TRACK_TITLE_SIZE, f) != NULL	)
+	{
+		remove_newline_if_exists(line);
+
+		auto new_node = (Node *)malloc(sizeof(Node));
+		new_node->next = NULL;
+		auto data = (Data *)malloc(sizeof(Data));
+		strcpy(data, line);
+		new_node->data = data;
+
+		insert_at(list, list_len(*list), new_node);
+
+		// Copy line to `new_node` and append `new_node` to `list`
+		// YOUR CODE HERE
+	}
+	fclose(f);
+	return list;
+}
+
+	/// Saves `list` contents to the file at `filename`.
+void save_file(const char *filename, Node *list)
+{
+	FILE *output_file = fopen(filename, "w"); //set as write file
+		
+
+	// Move through the list and save the tracks to the file
+	// Note: You have to cast the data to print the track to the file as follows:
+	// `*(Data *)current->data`, which is the same as `(char *)current->data`.
+	// We need this cast, because `data` is a pointer to everything (`void *`).
+	auto current = playlist;
+	while(current != NULL)
+	{
+	fprintf(output_file, "%s\n", *(Data *) current->data);
+	current = current->next; //hop videre i listen
+	}
+	// YOUR CODE HERE
+
+	fclose(output_file);
+}
+
+void print_tracks(const Node *const playlist) {
+	auto current = playlist;
+	for (size_t i = 1; current; i++, current = current->next)
+		printf("%2ld: %s\n", i, (char *)current->data);
+	}
+
+/*=============================================================================
+*                                Main function
+*=============================================================================*/
+
+int main() {
+	load_file(PLAYLIST_IN_PATH, &playlist);
+	puts("Loaded tracks:");
+	print_tracks(playlist);
+
+	// Deletion
+	size_t node_index_to_del = 4;
+	free(delete_at(&playlist, node_index_to_del));
+
+	// Insertion
+	Node node = {.data = "Tarkan – Şımarık 💋", .next = NULL};
+	insert_at(&playlist, 3, &node);
+
+	save_file(PLAYLIST_OUT_PATH, playlist);
+	}
+
+
+	/*
+
+	Use the template below and only provide code in YOUR CODE HERE blocks.
+
+	Document your project, but you don’t have to provide a flowchart.
+
+	Content in playlist.txt should be transformed and written to playlist-out.txt, so that you get the following output.
+	This modification is caused by highlighted lines in main.c.
+
+	*/
